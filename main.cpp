c2c2460be857a8d9ec4671b8e1223169477cd5e6
pklinh-21 <101463714+pklinh-21@users.noreply.github.com>
2022-04-18 08:34:08 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,289 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#include <iostream>
+#include <vector>
+
+bool init()
+{
+	if (SDL_Init(SDL_INIT_VIDEO) > 0)
+		std::cout << "SDL Failed " << SDL_GetError() << std::endl;
+	if (!(IMG_Init(IMG_INIT_PNG)))
+		std::cout << "IMG_init has failed " << SDL_GetError() << std::endl;
+	if (!(TTF_Init()))
+		std::cout << "TTF_init has failed" << SDL_GetError() << std::endl;
+	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+	return true;
+}
+
+bool SDLinit = init();
+
+RenderWindow window("3D pin ball", 640, 480);
+
+SDL_Texture* ballTexture = window.loadTexture("res/gfx/ball.png");
+SDL_Texture* holeTexture = window.loadTexture("res/gfx/hole.png");
+SDL_Texture* pointTexture = window.loadTexture("res/gfx/point.png");
+SDL_Texture* tileDarkTexture32 = window.loadTexture("res/gfx/tile32_dark.png");
+SDL_Texture* tileDarkTexture64 = window.loadTexture("res/gfx/tile64_dark.png");
+SDL_Texture* tileLightTexture32 = window.loadTexture("res/gfx/tile32_light.png");
+SDL_Texture* tileLightTexture64 = window.loadTexture("res/gfx/tile64_light.png");
+SDL_Texture* ballShadowTexture = window.loadTexture("res/gfx/ball_shadow.png");
+SDL_Texture* bgTexture = window.loadTexture("res/gfx/bg.png");
+SDL_Texture* uiBgTexture = window.loadTexture("res/gfx/UI_bg.png");
+SDL_Texture* levelTextBgTexture = window.loadTexture("res/gfx/levelText_bg.png");
+SDL_Texture* powerMeterTexture_FG = window.loadTexture("res/gfx/powermeter_fg.png");
+SDL_Texture* powerMeterTexture_BG = window.loadTexture("res/gfx/powermeter_bg.png");
+SDL_Texture* powerMeterTexture_overlay = window.loadTexture("res/gfx/powermeter_overlay.png");
+SDL_Texture* logoTexture = window.loadTexture("res/gfx/logo.png");
+SDL_Texture* click2start = window.loadTexture("res/gfx/click2start.png");
+SDL_Texture* endscreenOverlayTexture = window.loadTexture("res/gfx/end.png");
+SDL_Texture* splashBgTexture = window.loadTexture("res/gfx/splashbg.png");
+
+Mix_Chunk* chargeSfx = Mix_LoadWAV("res/sfx/charge.mp3");
+Mix_Chunk* swingSfx = Mix_LoadWAV("res/sfx/swing.mp3");
+Mix_Chunk* holeSfx = Mix_LoadWAV("res/sfx/hole.mp3");
+
+
+SDL_Color white = { 255, 255, 255 };
+SDL_Color black = { 0, 0, 0 };
+TTF_Font* font32 = TTF_OpenFont("res/font/font.ttf", 32);
+TTF_Font* font48 = TTF_OpenFont("res/font/font.ttf", 48);
+TTF_Font* font24 = TTF_OpenFont("res/font/font.ttf", 24);
+
+std::vector<Tile> loadTiles(int level)
+{
+	std::vector<Tile> temp = {};
+	switch(level) 
+	{
+	}
+	return temp;
+}
+
+int level = 0;
+std::vector<Tile> tiles = loadTiles(level);
+
+bool gameRunning = true;
+bool mouseDown = false;
+bool mousePressed = false;
+
+bool swingPlayed = false;
+bool secondSwingPlayed = false;
+
+
+SDL_Event event;
+
+int state = 0; 
+
+Uint64 currentTick = SDL_GetPerformanceCounter();
+Uint64 lastTick = 0;
+double deltaTime = 0;
+
+void loadLevel(int level)
+{
+	if (level > 4)
+	{
+		state = 2;
+		return;
+	}
+	balls[0].setVelocity(0, 0);
+	balls[1].setVelocity(0,0);
+    balls[0].setScale(1, 1);
+	balls[1].setScale(1, 1);
+	balls[0].setWin(false);
+	balls[1].setWin(false);
+
+	tiles = loadTiles(level);
+
+	switch (level)
+	{
+		//.............
+	}
+}
+
+const char* getStrokeText()
+{
+	int biggestStroke = 0;
+	if (balls[1].getStrokes() > balls[0].getStrokes())
+	{
+		biggestStroke = balls[1].getStrokes();
+	}
+	else
+	{
+		biggestStroke = balls[0].getStrokes();
+	}
+	std::string s = std::to_string(biggestStroke);
+	s = "STROKES: " + s;
+	return s.c_str();
+}
+
+const char* getLevelText(int side)
+{
+	//...................
+}
+
+void update()
+{
+	
+	lastTick = currentTick;
+	currentTick = SDL_GetPerformanceCounter();
+	deltaTime = (double)((currentTick - lastTick)*1000 / (double)SDL_GetPerformanceFrequency() );
+
+	mousePressed = false;
+	while (SDL_PollEvent(&event))
+	{
+		switch(event.type)
+		{
+		case SDL_QUIT:
+			gameRunning = false;
+			break;
+		case SDL_MOUSEBUTTONDOWN:
+			if (event.button.button == SDL_BUTTON_LEFT)
+			{
+				mouseDown = true;
+				mousePressed = true;
+			}
+			break;
+		case SDL_MOUSEBUTTONUP:
+			if (event.button.button == SDL_BUTTON_LEFT)
+			{
+				mouseDown = false;
+			}
+			break;
+		}
+	}
+
+	if (state == 1)
+	{
+		for (Ball& b : balls)
+		{
+			b.update(deltaTime, mouseDown, mousePressed, tiles, holes, chargeSfx, swingSfx, holeSfx);
+		}
+		if (balls[0].getScale().x < -1 && balls[1].getScale().x < -1)
+ 		{
+        	level++;
+			loadLevel(level);
+    	}
+	}
+}
+
+void graphics()
+{
+	window.clear();
+	window.render(0, 0, bgTexture);
+	for (Hole& h : holes)
+	{
+		window.render(h);
+	}
+	for (Ball& b : balls)
+	{
+		if (!b.isWin())
+		{
+			window.render(b.getPos().x, b.getPos().y + 4, ballShadowTexture);
+		}
+		for (Entity& e : b.getPoints())
+		{
+			window.render(e);
+		}
+		window.render(b);
+	}
+	for (Tile& t : tiles)
+	{ 
+		window.render(t);
+	}
+	for (Ball& b : balls)
+	{
+		for (Entity& e : b.getPowerBar())
+		{
+			window.render(e);
+		}
+		window.render(b.getPowerBar().at(0).getPos().x, b.getPowerBar().at(0).getPos().y, powerMeterTexture_overlay);
+		
+	}
+	if (state != 2)
+	{
+		//.................
+	}
+	else
+	{
+	//.....................
+	}
+	window.display();
+}
+
+void titleScreen()
+{
+	if (SDL_GetTicks() < 2000)
+	{
+		if (!swingPlayed)
+		{
+			Mix_PlayChannel(-1, swingSfx, 0);
+			swingPlayed = true;
+		}
+		while (SDL_PollEvent(&event))
+		{
+			switch(event.type)
+			{
+			case SDL_QUIT:
+				gameRunning = false;
+				break;
+			}
+		}
+         //..............
+	}
+	else
+	{
+		if (!secondSwingPlayed)
+		{
+			Mix_PlayChannel(-1, swingSfx, 0);
+			secondSwingPlayed = true;
+		}
+		lastTick = currentTick;
+		currentTick = SDL_GetPerformanceCounter();
+		deltaTime = (double)((currentTick - lastTick)*1000 / (double)SDL_GetPerformanceFrequency() );
+		while (SDL_PollEvent(&event))
+		{
+			switch(event.type)
+			{
+			case SDL_QUIT:
+				gameRunning = false;
+				break;
+			case SDL_MOUSEBUTTONDOWN:
+				if (event.button.button == SDL_BUTTON_LEFT)
+				{
+					Mix_PlayChannel(-1, holeSfx, 0);
+					state = 1;
+				}
+				break;
+			}
+		}
+		//.................
+	}
+}
+void game()
+{
+	if (state == 0)
+	{
+		titleScreen();
+	}
+	else
+	{
+		update();
+		graphics();
+	}
+}
+int main(int argc, char* args[])
+{
+	loadLevel(level);
+	while (gameRunning)
+	{
+		game();
+	}
+
+	window.cleanUp();
+	TTF_CloseFont(font32);
+	TTF_CloseFont(font24);
+	SDL_Quit();
+	TTF_Quit();
+	return 0;
+}
